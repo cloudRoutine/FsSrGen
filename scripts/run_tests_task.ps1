31af1703a8fb45b14eaa1e1069d9177b57116094
Jared Hester <jared.hester@gmail.com>
2016-10-16 23:11:50 -0400

    cleaned up posh scripts

--- scripts/run_tests_task.ps1
+++ scripts/run_tests_task.ps1
@@ -1,54 +1,36 @@
-#make path absolute
-$repoDir = Split-Path -parent (Split-Path -parent $PSCommandPath)
+# exists the script if the preceeding command failed
+function check-last { if(-not $?){ exit 1 }}
 
-Push-Location
+$scriptDir = split-path $script:MyInvocation.MyCommand.Path
+$repoDir = split-path -parent $scriptdir
+$testDir = "$repoDir\test\use-fssrgen-as-msbuild-task"
 
-cd "$repoDir\test\use-fssrgen-as-msbuild-task"
-
-
-# restore package
-
-& "$repoDir\.nuget\NuGet.exe" restore .\packages.config -PackagesDirectory packages -Source "$repoDir\bin\packages"
-if (-not $?) {
-	exit 1
-}
+# restore testproject and tools from package 
+dotnet restore $testDir   -f "$repoDir\bin\packages\"
+check-last  
 
 # run tool
-$testProjDir = $pwd
-msbuild FsSrGenAsMsbuildTask.msbuild /verbosity:detailed 
-if (-not $?) {
-	exit 1
-}
+msbuild "$testdir\FsSrGenAsMsbuildTask.msbuild" /verbosity:detailed 
+check-last  
 
-
+$stored = $pwd
 # restore test project
-
-dotnet restore
-if (-not $?) {
-	exit 1
-}
-
+#dotnet restore $testDir
+#check-last  
+cd $testDir
 
 # build
-
-dotnet --verbose build
-if (-not $?) {
-	exit 1
-}
+dotnet -v build 
+check-last  
 
 # run tests netcoreapp 1.0
-
-dotnet run --framework netcoreapp1.0 -- --verbose
-if (-not $?) {
-	exit 1
-}
+dotnet run  --framework netcoreapp1.0 -- --verbose
+check-last  
 
 # run tests net45
-
 .\bin\Debug\net45\win7-x64\use-fssrgen-as-msbuild-task.exe --verbose
-if (-not $?) {
-	exit 1
-}
+check-last  
 
-Pop-Location
+cd $stored
+
 exit 0
